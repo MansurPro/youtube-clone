f8f14fdf2bf7ec7bec36c1b19f3cf85c2548a496
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-04-20 11:01:12 -0500

    First commit - youtube clone

---
+++ src/components/Videos.jsx
@@ -0,0 +1,11 @@
+import { Stack, Box } from "@mui/material";
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="">
+
+    </Stack>
+  )
+}
+
+export default Videos
